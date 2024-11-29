1f902148e33c01f3e4bd085e03e8d850e2d991dd
Gian C Fava <gianfava@gmail.com>
2024-11-29 15:49:30 -0300

    (29/11) ajustes no Produto Schema

--- api/v1/produtos/produto-schema.js
+++ api/v1/produtos/produto-schema.js
@@ -4,7 +4,23 @@ const produtoSchema = Joi.object({
     id: Joi.string().required(),
     nome: Joi.string().required(),
     descricao: Joi.string(),
-    
+    categoria: Joi.string(),
+    marca: Joi.string(),
+    preco: Joi.number().positive(),
+    quantidadeEstoque: Joi.number().integer(),
+    codigoBarras: Joi.string(),
+    dimensoes: Joi.object({
+        altura: Joi.number(),
+        largura: Joi.number(),
+        profundidade: Joi.number(),
+        unidadeMedida: Joi.string(),
+    }),
+    peso: Joi.object({
+        valor: Joi.number(),
+        unidadeMedida: Joi.string(),
+    }),
+    status: Joi.string(),
+    dataCadastro: Joi.date(),
 });
 
 const createProduto = { payload: produtoSchema };
